de478c25cc032c74221eece9f211b128e6e5aeb4
Aadesh Wasamkar <aadeshwasamkar@gmail.com>
2025-10-06 19:12:56 +0530

    added .env file

--- src/app.js
+++ src/app.js
@@ -15,13 +15,34 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-connectDB()
-  .then(() => {
-    console.log("Database connected successfully");
-    app.listen(7777, () => {
-      console.log("server is running on port 7777");
-    });
-  })
-  .catch((err) => {
-    console.log("Database connection failed", err);
-  });
+//get a user data by finding via first user with given email
+app.get("/userData", async (req, res) => {
+  try {
+    const users = await User.find({ email: "sydney@sweeny.com" });
+    if (users.length === 0) {
+      res.status(500).send("something went wrong");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+
+//get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length === 0) {
+      res.status(500).send("something went wrong");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.all("/", (req, res) => {
+  res.status(404).send("Hello");
+});
